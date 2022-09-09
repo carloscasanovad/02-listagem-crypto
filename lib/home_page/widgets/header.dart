c929c0993501f4f52e6d77c3fa776cc1f58bba0e
carloscasanovad <carlos.casanova@warren.com.br>
2022-09-09 16:31:45 -0300

    fix/EV-739/ Adding space between Header & CryptoCards

--- lib/home_page/widgets/header.dart
+++ lib/home_page/widgets/header.dart
@@ -21,53 +21,59 @@ class _HeaderState extends ConsumerState<Header> {
   @override
   Widget build(BuildContext context) {
     final bool visibility = ref.watch(visibilityProvider);
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: kDefaultPadding,
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Carteira',
-                    style: kTitleTextStyle,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      ref.read(visibilityProvider.notifier).state = !visibility;
-                      setState(() {});
-                    },
-                    icon: const Icon(
-                      Icons.remove_red_eye_rounded,
-                      size: 26,
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  const Text(
-                    "US\$ ",
-                    style: kTitleTextStyle,
-                  ),
-                  ImageFiltered(
-                    imageFilter: visibility
-                        ? ImageFilter.blur(sigmaX: 0, sigmaY: 0)
-                        : ImageFilter.blur(sigmaX: 8, sigmaY: 8),
-                    child: Text(
-                      currency.format(userBalance),
+    return Container(
+      margin: const EdgeInsets.only(
+        bottom: 44,
+      ),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: kDefaultPadding,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Carteira',
                       style: kTitleTextStyle,
                     ),
-                  ),
-                ],
-              )
-            ],
+                    IconButton(
+                      onPressed: () {
+                        ref.read(visibilityProvider.notifier).state =
+                            !visibility;
+                        setState(() {});
+                      },
+                      icon: const Icon(
+                        Icons.remove_red_eye_rounded,
+                        size: 26,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    const Text(
+                      "US\$ ",
+                      style: kTitleTextStyle,
+                    ),
+                    ImageFiltered(
+                      imageFilter: visibility
+                          ? ImageFilter.blur(sigmaX: 0, sigmaY: 0)
+                          : ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+                      child: Text(
+                        currency.format(userBalance),
+                        style: kTitleTextStyle,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
